98265fb0f8e6afc89f75feba386598a75d200136
Igor Lopes <igor@nucleus.eti.br>
2017-07-21 22:33:26 -0300

    Add skeleton

---
+++ index.js
@@ -0,0 +1,5 @@
+var credentials = {
+  user: 'test'
+}
+
+exports.Moip = require('./lib/moip')(credentials);
---
+++ specs/moip.specs.js
@@ -0,0 +1,15 @@
+var chai = require('chai');
+var should = chai.should();
+
+var Moip = require('../index').Moip;
+
+var moip = new Moip({
+  'user': "test"
+});
+
+describe('Moip Test', function() {
+
+    it('basic test', function(done) {
+        done();
+    });
+});
