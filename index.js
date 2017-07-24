77044d02640279b27ae1abadb88b54eb0e3bbb2f
Igor Lopes <igor@nucleus.eti.br>
2017-07-23 22:17:48 -0300

    Add customer

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var credentials = {
-  user: 'test'
-}
+module.exports = function(credentials) {
 
-exports.Moip = require('./lib/moip')(credentials);
+    var basicAuth = "Basic " + new Buffer(credentials.token + ":" + credentials.key).toString("base64");
+
+    return {
+        Customer: require('./lib/customer')(basicAuth)
+    };
+};
--- specs/moip.specs.js
+++ specs/moip.specs.js
@@ -1,15 +1,28 @@
 var chai = require('chai');
 var should = chai.should();
 
-var Moip = require('../index').Moip;
-
-var moip = new Moip({
-  'user': "test"
+var Moip = require('../index.js')({
+    token: '01010101010101010101010101010101',
+    key: 'ABABABABABABABABABABABABABABABABABABABAB'
 });
 
-describe('Moip Test', function() {
+var customer = {};
 
-    it('basic test', function(done) {
-        done();
+describe('Moip Customers', function() {
+
+    it('Create Customer', function(done) {
+        Moip.Customer.create(function(error, body, response) {
+            console.log(response.statusCode);
+            console.log(body);
+            done();
+        });
+    });
+
+    it('Get Customer', function(done) {
+      Moip.Customer.get('CUS-UNTR67Z9JCAJ', function(error, body, response) {
+          console.log(response.statusCode);
+          console.log(body);
+          done();
+      });
     });
 });
