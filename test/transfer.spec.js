955e14b108c38e714f93b90ae776a6eaa9b708a5
karlsmarx <karlmarxti@gmail.com>
2018-11-24 01:18:26 -0200

    resolving identation

--- test/transfer.spec.js
+++ test/transfer.spec.js
@@ -10,39 +10,39 @@ const moipAccount = 'MPA-CULBBYHD11'
 let transferID
 
 describe('Transfer', () => {
-	it('Should successfully transfer', (done) => {
-		moip.transfer.create(transferModel)
-			.then(({ body }) => {
-				body.should.have.property('id')
-				transferID = body.id
-				done()
-			}).catch(done)
-	})
+  it('Should successfully transfer', (done) => {
+    moip.transfer.create(transferModel)
+      .then(({ body }) => {
+        body.should.have.property('id')
+        transferID = body.id
+        done()
+      }).catch(done)
+  })
 
-	it('Should successfully get one transfer', (done) => {
-		moip.transfer.getOne(transferID)
-			.then(({ body }) => {
-				body.should.have.property('id')
-				done()
-			}).catch(done)
-	})
+  it('Should successfully get one transfer', (done) => {
+    moip.transfer.getOne(transferID)
+      .then(({ body }) => {
+        body.should.have.property('id')
+        done()
+      }).catch(done)
+  })
 
-	it('Should fail to get a transfer', (done) => {
-		moip.transfer.getOne('non-existent-id')
-			.catch(() => done())
-	})
+  it('Should fail to get a transfer', (done) => {
+    moip.transfer.getOne('non-existent-id')
+      .catch(() => done())
+  })
 
-	it('Should successfully get all tranfers', (done) => {
-		moip.transfer.getAll(moipAccount)
-			.then(() => {
-				done()
-			}).catch(done)
-	})
+  it('Should successfully get all tranfers', (done) => {
+    moip.transfer.getAll(moipAccount)
+      .then(() => {
+        done()
+      }).catch(done)
+  })
 
-	it('Should successfully reverse a transfer', (done) => {
-		moip.transfer.reverse(transferID)
-			.then(() => {
-				done()
-			}).catch(done)
-	})
+  it('Should successfully reverse a transfer', (done) => {
+    moip.transfer.reverse(transferID)
+      .then(() => {
+        done()
+      }).catch(done)
+  })
 })
